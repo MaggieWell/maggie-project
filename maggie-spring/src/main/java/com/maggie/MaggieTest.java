76a5f5619c4671242fc23b47faab6405be3cb8ab
maggie.hao <mengjie.hao@baozun.com>
2019-04-15 10:45:50 +0800

    test

---
+++ maggie-spring/src/main/java/com/maggie/MaggieTest.java
@@ -0,0 +1,8 @@
+package main.java.com.maggie;
+
+/**
+ * @author Maggie.Hao
+ * @date 2019/4/15 10:42
+ */
+public class MaggieTest {
+}
---
+++ migo-api/src/main/java/com/migo/resolver/MaggieTest.java
@@ -0,0 +1,11 @@
+package main.java.com.maggie;
+
+/**
+ * @author Maggie.Hao
+ * @date 2019/4/15 10:42
+ */
+public class MaggieTest {
+    public static void main(String[] args){  
+        System.out.println("1111111111111111");
+    } 
+}

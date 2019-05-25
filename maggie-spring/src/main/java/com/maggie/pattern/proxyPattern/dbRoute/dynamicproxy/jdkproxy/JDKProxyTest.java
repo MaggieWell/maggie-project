1e96f0e5bf67fb6551176edf007491f04e20059e
Maggie.Hao <1083719328@qq.com>
2019-05-25 22:06:04 +0800

    媒婆动态代理案例

---
+++ maggie-spring/src/main/java/com/maggie/pattern/proxyPattern/dbRoute/dynamicproxy/jdkproxy/JDKProxyTest.java
@@ -0,0 +1,19 @@
+package com.maggie.pattern.proxyPattern.dbRoute.dynamicproxy.jdkproxy;
+
+import com.maggie.pattern.proxyPattern.Person;
+
+/**
+ * Description:媒婆
+ * author:MaggieHao
+ * Date:2019-05-19
+ * Time:20:25
+ */
+public class JDKProxyTest{
+    public static void main(String[] args) {
+
+        Person obj=(Person)new JDBCMatchmaker().getInstance(new Girl());
+        obj.findLove();
+    }
+
+
+}

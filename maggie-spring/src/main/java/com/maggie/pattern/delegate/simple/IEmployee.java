7beb12ce8b84bcc2c6b74cd853badea0aea4ecc8
Maggie.Hao <1083719328@qq.com>
2019-06-25 23:11:53 +0800

    委派模式

---
+++ maggie-spring/src/main/java/com/maggie/pattern/delegate/simple/IEmployee.java
@@ -0,0 +1,11 @@
+package com.maggie.pattern.delegate.simple;
+
+/**
+ * Description:员工，有很多个，是负责执行任务的
+ * author:MaggieHao
+ * Date:2019-06-25
+ * Time:22:45
+ */
+public interface IEmployee {
+    void doing(String command);
+}
